805a20e3a82e2641eb5c0a1e7ef13ce2839edac1
Eugene Griaznov <evgeniy.gryaznov@gmail.com>
2021-01-26 13:29:47 +0700

    Show value from app config

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace blog_arm_templates.Pages
@@ -11,15 +12,19 @@ namespace blog_arm_templates.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _config;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public String LinkToBlog { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
 
         public void OnGet()
         {
-
+            LinkToBlog = _config.GetValue<String>("Link_to_blog");
         }
     }
 }
